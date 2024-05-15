6091d9e651f2174666248fbb575e079213c8062f
Hans <hans.miikael@gmail.com>
2024-05-15 19:20:00 +0300

    Query for actual data from the DB

--- second/server/resolvers.js
+++ second/server/resolvers.js
@@ -1,18 +1,9 @@
+import { getJobs } from './db/jobs.js';
+
 export const resolvers = {
     Query: {
-        job: () => {
-            return [
-                {
-                    id: 'test-id',
-                    title: 'The title',
-                    description: 'The description'
-                },
-                {
-                    id: 'test-id-2',
-                    title: 'The title 2',
-                    description: 'The description 2'
-                }
-            ];
+        jobs: async () => {
+           return await getJobs();
         }
     }
 };
