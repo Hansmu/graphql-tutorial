f26e44d2822c632de32c9492834cabdc6d221770
Hans <hans.miikael@gmail.com>
2024-05-15 19:20:30 +0300

    Simplify the data query

--- second/server/resolvers.js
+++ second/server/resolvers.js
@@ -2,8 +2,6 @@ import { getJobs } from './db/jobs.js';
 
 export const resolvers = {
     Query: {
-        jobs: async () => {
-           return await getJobs();
-        }
+        jobs: getJobs
     }
 };
