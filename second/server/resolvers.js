ae11116e4031cc9c019d7183ca5c92e2a42f5cf0
Hans <hans.miikael@gmail.com>
2024-05-15 19:05:07 +0300

    Add example of array definitions

--- second/server/resolvers.js
+++ second/server/resolvers.js
@@ -1,11 +1,18 @@
 export const resolvers = {
     Query: {
         job: () => {
-            return {
-                id: 'test-id',
-                title: 'The title',
-                description: 'The description'
-            }
+            return [
+                {
+                    id: 'test-id',
+                    title: 'The title',
+                    description: 'The description'
+                },
+                {
+                    id: 'test-id-2',
+                    title: 'The title 2',
+                    description: 'The description 2'
+                }
+            ];
         }
     }
 };
