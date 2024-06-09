e42fba42c2e839a42a2ce0d5597b26925b8c315d
Hans <hans.miikael@gmail.com>
2024-06-09 18:35:15 +0300

    Add cache updating via mutation return

--- subscriptions/client/src/lib/graphql/hooks.js
+++ subscriptions/client/src/lib/graphql/hooks.js
@@ -5,8 +5,18 @@ export function useAddMessage() {
   const [mutate] = useMutation(addMessageMutation);
 
   const addMessage = async (text) => {
+    // So when we're using ApolloClient, by default things go into the cache
+    // We start the page off with a useQuery, which gets the messages that currently exists
+    // If we post a new message, we want to have it show up instantly
+    // For that we could send a new query or simply update the cache with the returned object of our mutation query
     const { data: { message } } = await mutate({
       variables: { text },
+      update: (cache, { data }) => {
+        const newMessage = data.message;
+        cache.updateQuery({ query: messagesQuery }, ({ messages }) => {
+          return { messages: [...messages, newMessage] };
+        });
+      },
     });
     return message;
   };
