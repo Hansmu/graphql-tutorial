8de5c5c043b96a81cb47ab7c81447b62072d345d
Hans <hans.miikael@gmail.com>
2024-06-11 17:32:17 +0300

    Set up the client connection for web sockets

--- subscriptions/client/src/lib/graphql/client.js
+++ subscriptions/client/src/lib/graphql/client.js
@@ -1,7 +1,18 @@
-import { ApolloClient, ApolloLink, concat, createHttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloLink, concat, createHttpLink, InMemoryCache, split } from '@apollo/client';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { Kind, OperationTypeNode } from 'graphql';
+import { createClient as createWsClient } from 'graphql-ws';
 import { getAccessToken } from '../auth';
 
-const httpLink = createHttpLink({ uri: 'http://localhost:9000/graphql' });
+const httpLink = concat(authLink, createHttpLink({
+  uri: 'http://localhost:9000/graphql'
+}));
+
+// Able to process GraphQL over Websocket
+const wsLink = new GraphQLWsLink(createWsClient({
+  url: 'ws://localhost:9000/graphql',
+}));
 
 const authLink = new ApolloLink((operation, forward) => {
   const accessToken = getAccessToken();
@@ -14,6 +25,14 @@ const authLink = new ApolloLink((operation, forward) => {
 });
 
 export const apolloClient = new ApolloClient({
-  link: concat(authLink, httpLink),
+  // Since WebSockets require a bit of a different way of handling it, then we split the traffic
+  // If it's a subscription, then it's a websocket and has to be handled by the WS Link
+  link: split(isSubscription, wsLink, httpLink),
   cache: new InMemoryCache(),
 });
+
+function isSubscription(operation) {
+  const definition = getMainDefinition(operation.query);
+  return definition.kind === Kind.OPERATION_DEFINITION
+    && definition.operation === OperationTypeNode.SUBSCRIPTION;
+}
